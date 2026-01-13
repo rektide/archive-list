adc709541f1c02101a70f991cf00f200e180399c
rektide <rektide+scm@voodoowarez.com>
2026-01-12 22:04:15 -0500

    feat: implement rate interval subsystem with 1.5x velocity

--- src/util/ratelimit.rs
+++ src/util/ratelimit.rs
@@ -5,6 +5,8 @@ use governor::{
 };
 
 pub fn create_rate_limiter(requests_per_second: u32) -> RateLimiter<NotKeyed, InMemoryState, DefaultClock> {
-    let quota = Quota::per_second(u32::max(1, requests_per_second).try_into().unwrap());
+    let velocity = (requests_per_second as f64 * 1.5) as u32;
+    let requests = u32::max(1, velocity);
+    let quota = Quota::per_second(requests.try_into().unwrap());
     RateLimiter::direct(quota)
 }
